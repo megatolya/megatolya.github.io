9cb1a4a43f819ecee11e607895a1eb3b1763382b
Anatoly Ostrovsky <megatolya@yandex-team.ru>
2015-07-31 13:16:27 +0300

    ..

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,9 +6,8 @@ var Footer = React.createClass({
     render: function () {
         return (
             <footer>
-                <div className="lang-changer">
-                    <a href="/en">Change language</a>
-                    <Lang lang="gb"/>
+                <div className="follow">
+                    <iframe src="https://ghbtns.com/github-btn.html?user=megatolya&type=follow&count=true&size=large" frameBorder="0" scrolling="0" width="220px" height="30px"></iframe>
                 </div>
                 <small className="copyright">© 2014&ndash;{new Date().getFullYear()} <a href="mailto:forgetthisbox@gmail.com">{this.props.author}</a></small>
             </footer>
@@ -17,3 +16,8 @@ var Footer = React.createClass({
 });
 
 module.exports = Footer;
+                //<div className="lang-changer">
+                    //<a href="/en">Change language</a>
+                    //<Lang lang="gb"/>
+                //</div>
+                //<
