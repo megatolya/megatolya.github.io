fcf43187c612c096047a86961a76830fb7fb489c
Anatoly Ostrovsky <megatolya@yandex-team.ru>
2015-07-31 13:21:14 +0300

    ..

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -7,7 +7,7 @@ var Footer = React.createClass({
         return (
             <footer>
                 <div className="follow">
-                    <iframe src="https://ghbtns.com/github-btn.html?user=megatolya&type=follow&count=true&size=large" frameBorder="0" scrolling="0" width="220px" height="30px"></iframe>
+                    <iframe src="https://ghbtns.com/github-btn.html?user=megatolya&type=follow&count=true" frameBorder="0" scrolling="0" width="160px" height="25px"></iframe>
                 </div>
                 <small className="copyright">© 2014&ndash;{new Date().getFullYear()} <a href="mailto:forgetthisbox@gmail.com">{this.props.author}</a></small>
             </footer>
