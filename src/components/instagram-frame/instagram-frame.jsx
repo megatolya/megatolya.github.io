24c4e19df1a23e61cb0c89f31dcc6be5830d51b4
Anatoly Ostrovsky <megatolya@yandex-team.ru>
2015-07-31 14:03:56 +0300

    ..

--- src/components/instagram-frame/instagram-frame.jsx
+++ src/components/instagram-frame/instagram-frame.jsx
@@ -12,12 +12,10 @@ var InstagramFrame = React.createClass({
         return (
             <noindex>
                 <script src="//instansive.com/widget/js/instansive.js"></script>
-                <iframe
-                    src="//instansive.com/widgets/2fd0cfa465d9314a0b9f2ea842ec9cccab51cbae.html"
-                    id="instansive_2fd0cfa465"
-                    name="instansive_2fd0cfa465"
-                    scrolling="no"
-                    allowTransparency="true"
+                <iframe src="//instansive.com/widgets/2f2ea965452a93353f77df071b06a2f02cfcae4e.html"
+                    id="instansive_2f2ea96545"
+                    name="instansive_2f2ea96545"
+                    scrolling="no" allowTransparency="true"
                     className="instansive-widget"
                     style={styles}
                 ></iframe>
