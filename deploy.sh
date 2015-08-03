af0c87f7cc3c696012aafb1d48f274490a5d995b
Travis CI <forgetthisbox@gmail.com>
2015-08-03 23:31:01 +0300

    ..

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,6 @@
 set -e # exit with nonzero exit code if anything fails
 
 # run our compile script, discussed above
-node build.js
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
@@ -11,8 +10,9 @@ git config user.email "forgetthisbox@gmail.com"
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git init
-git add .
 git checkout master
-git commit -a -m "Auto build #$TRAVIS_BUILD_NUMBER"
+node build.js
+git add .
+git commit -a -m "Auto build #$TRAVIS_BUILD_NUMBER" -m '[ci skip]'
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
 
