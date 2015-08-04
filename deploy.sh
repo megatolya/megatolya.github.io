beba24a1e7f798f2bf3a4ef14280c2c47ba5400d
Anatoly Ostrovsky <forgetthisbox@gmail.com>
2015-08-04 10:22:45 +0300

    ..

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-set -e # exit with nonzero exit code if anything fails
+set -e
 ./node_modules/.bin/bower install
+./node_modules/.bin/bower update
 
-# run our compile script, discussed above
-
-# inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
 git config user.email "forgetthisbox@gmail.com"
 
-# The first and only commit to this new Git repo contains all the
-# files present with the commit message "Deploy to GitHub Pages".
 git init
 git checkout master
 node build.js
 git add .
 git commit -a -m "Auto build #$TRAVIS_BUILD_NUMBER" -m '[ci skip]'
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
-
