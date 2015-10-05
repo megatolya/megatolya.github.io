61846d136e3ab5f3e5f0eccbf8331ddd7ee80f5f
megatolya <forgetthisbox@gmail.com>
2015-10-05 20:30:47 +0300

    ..

--- local-connection.js
+++ local-connection.js
@@ -1,24 +1,13 @@
 (function (global) {
-    // For version detection, set to min. required Flash Player version, or 0 (or 0.0.0), for no version detection.
-    var swfVersionStr = '0.0.0';
-    // To use express install, set to playerProductInstall.swf, otherwise the empty string.
-    var xiSwfUrlStr = '';
+    var connections = {};
 
-    var params = {};
+    global.onLocalConnection = function (name, data) {
+        console.log('onLocalConnection', name);
+        Object.keys(connections).forEach(function (connectionName) {
+            var connection = connections[connectionName];
 
-    params.quality = 'high';
-    params.allowscriptaccess = 'always';
-    params.allowfullscreen = 'true';
-
-    var attributes = {};
-
-    attributes.id = 'lc_debug';
-    attributes.name = 'lc_debug';
-    params.allowscriptaccess = 'always';
-    attributes.align = 'middle';
-
-    global.onLocalConnection = function (message) {
-        alert(message);
+            connection._onMessage(name, data);
+        });
     };
 
     var ready = false;
@@ -34,47 +23,53 @@
     }
 
 
-    window.addEventListener('DOMContentLoaded', function () {
-        setTimeout(function () {
-            ready = true;
-            queue.forEach(function (callback) {
-                callback();
-            });
-        }, 1000);
-    });
+    var OBJECT_ID = 'local-connection';
 
-
-
-    global.LocalConnection = function LocalConnection(inputChannel, outputChannel) {
+    global.LocalConnection = function LocalConnection(outputChannel, inputChannel) {
         this._handlers = {};
 
         window.addEventListener('DOMContentLoaded', function () {
             var elem = document.createElement('div');
-            var id = 'lc';
+            var id = 'local-connection-placeholder';
+
             elem.id = id;
             elem.setAttribute('name', id);
             document.body.appendChild(elem);
 
-            var flashvars = {
-                onMessage: 'onLocalConnection',
-                'in_channel': inputChannel,
-                'out_channel': outputChannel
-            };
-            console.log('flashvars', flashvars);
-            console.log('attributes', attributes);
-
             swfobject.embedSWF(
                 'http://tolya.me/lc_debug.swf',
-                'lc',
+                'local-connection-placeholder',
                 '100%',
                 '100%',
-                swfVersionStr,
-                xiSwfUrlStr,
-                flashvars,
-                params,
-                attributes
+                // For version detection, set to min. required Flash Player version, or 0 (or 0.0.0), for no version detection.
+                '0.0.0',
+                // To use express install, set to playerProductInstall.swf, otherwise the empty string.
+                '',
+                {
+                    onMessage: 'onLocalConnection',
+                    'in_channel': inputChannel,
+                    'out_channel': outputChannel
+                },
+                {
+                    quality: 'high',
+                    allowScriptAccess: 'always',
+                    allowFullScreen: 'true'
+                },
+                {
+                    id: OBJECT_ID,
+                    name: OBJECT_ID,
+                    align: 'middle'
+                }
             );
-            console.log('embedSWF');
+
+
+            // TODO onload
+            setTimeout(function () {
+                ready = true;
+                queue.forEach(function (callback) {
+                    callback();
+                });
+            }, 300);
         });
     };
 
@@ -89,19 +84,24 @@
             handlers.splice(handlers.indexOf(handler), 1);
         },
 
-        broadcast: function (eventName, eventData) {
-            // todo queue
+        emit: function (eventName, data) {
             onReady(function () {
-                var emitter = document.getElementById('lc');
-                emitter.send(eventData, eventData);
-                console.log('sent');
+                var emitter = document.getElementById(OBJECT_ID);
+                emitter.send(eventName, data);
+            });
+        },
+
+        _onMessage: function (eventName, data) {
+            var handlers = this._handlers[eventName] = this._handlers[eventName] || [];
+            handlers.forEach(function (handler) {
+                handler(data);
             });
         }
     };
 
-    var connections = {};
-
-    global.getLocalConnection = function (id) {
-        return connections[id] = connections[id] || new LocalConnection(id);
+    global.getLocalConnection = function (input, output) {
+        var id = input + output;
+        var output = connections[id] = connections[id] || new LocalConnection(input, output);
+        return output;
     };
 })(window);
