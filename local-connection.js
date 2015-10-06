091b2611d0c54a168af63ad470593ff9a1fbb50b
Anatoly Ostrovsky <forgetthisbox@gmail.com>
2015-10-06 14:55:59 +0300

    Update local-connection.js

--- local-connection.js
+++ local-connection.js
@@ -3,7 +3,8 @@
     var loaded = false;
 
     window.onLocalConnection = function (name, data) {
-        alert('incoming message ' + JSON.stringify(data));
+        alert('incoming message!');
+        alert('incoming message name = ' + name);
         Object.keys(connections).forEach(function (connectionName) {
             var connection = connections[connectionName];
 
